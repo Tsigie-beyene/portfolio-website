0d6d89d89d254d70e56dd2a6379f56b32a541203
Tsigie-beyene <tsigiebey29@gmail.com>
2025-04-15 17:06:54 +0300

    finalizing the hero section adding framer motion in the hero secttion

--- src/components/sub/Hero.jsx
+++ src/components/sub/Hero.jsx
@@ -1,12 +1,53 @@
 'use client'
 import Image from "next/image";
+import { heroIcons } from "@/assets/index";
+import { useMotionValue, useTransform,motion,useSpring} from "framer-motion";
+import { useState } from "react";
 
 export default function Hero() {
-  return (
-    <div className="h-screen grid place-items-center">
+
+   const [windowOffset,setWindowOffset] =useState({innerWidth:0 , innerHeight:0})
+   const [mouseMove,setMouseMove] =useState(false)
+   const [buttonHover,setButtonHover]= useState(false)
+   const x =useMotionValue(0)
+   const y =useMotionValue(0)
+
+   const handleMouseMove=(e)=>{
+      const {clientX,clientY} = e
+      x.set(clientX)
+      y.set(clientY)
+
+      // console.log(clientX,clientY,x,y)
+   }
+
+   const handleMouseEnter =() =>{
+       setWindowOffset({innerWidth:window.innerWidth, innerHeight:window.innerHeight})
+       setMouseMove(true)
+
+   }
+  const {innerWidth,innerHeight} =windowOffset
+
+   const xSpring =useSpring(x,{stiffness:100, damping:10})
+   const ySpring =useSpring(y,{stiffness:100, damping:10}) 
+ 
+
+  const rotateY= useTransform(xSpring,[0,innerWidth],[-30,30])
+  const rotateX=useTransform(ySpring,[0,innerHeight],[10,-50])
+
+  return (  
+    <div className="h-screen grid place-items-center" 
+    onMouseMove={handleMouseMove}
+    onMouseEnter={handleMouseEnter}
+    >
      <div>
           <div className="felx flex-col items-center justify-center gap-y-3 font-light capitalize ">
-             <div className="flex items-center justify-center">
+             <motion.div 
+             className="flex items-center justify-center" 
+             style ={{
+                 rotateX: mouseMove ? rotateX: 0,
+                 rotateY: mouseMove ? rotateY: 0,
+                 transition: '0.1s',
+             }}>
                 <Image 
                 src={'/person.png'} 
                 alt="person Image" 
@@ -15,15 +56,31 @@ export default function Hero() {
                 priority={true}
                 className="h-auto w-[150px]"
                 />
-                <span className="absolute text-3xl font-semibold text-white"> Hi </span>
-            </div>
+                <motion.span className="absolute text-3xl font-semibold text-white"
+                 initial={{scale:0}}
+                 animate={{opacity: buttonHover? 0:1,
+                  scale:buttonHover? 2:0,
+                  y: buttonHover? -40: 0,
+                 }}
+                 transition={{opacity:{delay:0.4}}}
+                 > Hi </motion.span>
+            </motion.div>
             <h1 className=" text-center text-3xl  font-bold tracking-wider text-gray-500">My Name is Tsigie Beyene &</h1>
             <p className=" text-lg tracking-wider text-gray-700">I like learning and building every day😊</p>
          </div>
-         <div>
-            <a href="#">Icon</a>
+         <div className="mt-8 flex justify-center gap-x-10 text-3xl text-yellow-600">
+         {heroIcons.map((icon, i) => (
+         <a href="#" key={i} className="rounded-lg hover:bg-red-400 hover:text-white transition-colors">
+           {icon}
+            </a>
+             ))}
+
          </div>
-         <a href="#" className="mx-auto block w-max rounded-lg bg-red-400 px-3 py-1 font-light capitalize tracking-wider">Talk to me</a>
+         <a href="#" className="mx-auto mt-7 block w-max rounded-lg bg-red-400 px-3 py-1 font-light capitalize tracking-wider text-white hover:bg-red-500 transition-colors"
+         onMouseEnter={()=>setButtonHover(true)}
+         onMouseLeave={()=>setButtonHover(false)}
+         
+         >Talk to me</a>
     </div>    
     </div>
   );
