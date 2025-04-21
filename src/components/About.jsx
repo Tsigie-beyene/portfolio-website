a3ac6f7557072fa9a95a4a4c46dacba5200caa40
Tsigie-beyene <tsigiebey29@gmail.com>
2025-04-21 16:13:33 +0300

    dawnload cv

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,18 +6,19 @@ import Achievements from "./sub/Achievements"
 import { aboutData,aboutText,downloadIcon,arrowLeftIcon } from "@/assets"
 export default function About() {
   return (
-    <div className="min-h-screen px-96 flex flex-col items-center justify-center">
+<div className="min-h-screen px-50 flex flex-col items-center justify-center">
      <Heading text={"About Me"}/> 
       <div className="w-full flex items-center justify-between  md:justify-center">
-      <Image src={'/about-me.png'} alt="About Image" width={400} height={400} className="w-[300px] lg:w-[200px]  "/>
-      <div className="relative max-w-[800px] rounded-xl bg-zinc-100 p-5 text-justify">
-        <span className="absolute -left-5 top-30 scale-[2.5] text-zinc-100  ">{arrowLeftIcon}</span>
+      <Image src={'/about-me.png'} alt="About Image" width={400} height={400} className="w-[300px] lg:w-[200px] md:hidden sm:hidden  "/>
+      <div className=" relative max-w-[800px] rounded-xl bg-zinc-100 p-5 text-justify">
+        <span className="absolute -left-5 top-25 scale-[2.5] text-zinc-100 md:hidden ">{arrowLeftIcon}</span>
         <p className="text-lg font-light text-gray-700 first-letter:pl-3 lg:text-[16px] sm:text-[14px]"> {aboutText}</p>
-        <a href="/nick-cv.pdf" downloads="">
+        <a href="/nick-cv.pdf" downloads="" className="w-max flex items-center gap-x-2 mt-6 rounded-full border border-gray-300
+        bg-red-400 px-3 py-2 font-light text-white hover:bg-red-500 transition-colors">
           <span>Download CV</span>
-          <span>Download icon</span>
+          <span className="text-2xl">{downloadIcon}</span>
 
-
+                  
         </a>
       </div>
       </div>
