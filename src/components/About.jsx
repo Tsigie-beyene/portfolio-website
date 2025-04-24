5a7fadc5882e182ee46e65ef629313fe954ede7e
Tsigie-beyene <tsigiebey29@gmail.com>
2025-04-24 12:05:20 +0300

    adding Education and experiance section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ import Achievements from "./sub/Achievements"
 import { aboutData,aboutText,downloadIcon,arrowLeftIcon } from "@/assets"
 export default function About() {
   return (
-<div className="min-h-screen px-50 flex flex-col items-center justify-center">
+<div className="min-h-screen px-60 flex flex-col items-center justify-center">
      <Heading text={"About Me"}/> 
       <div className="w-full flex items-center justify-between  md:justify-center">
       <Image src={'/about-me.png'} alt="About Image" width={400} height={400} className="w-[300px] lg:w-[200px] md:hidden sm:hidden  "/>
--- src/components/Experiance.jsx
+++ src/components/Experiance.jsx
@@ -1,6 +1,51 @@
 'use client'
+
+import Heading from "./sub/Heading"
+import Image from "next/image"
+
 export default function Experiance () {
   return (
-    <div>Experiance</div>
+    <div className="py-20 px-80 relative
+     ">
+      <Heading text={'Experiance & Education'}/>
+      <Image src={"/education.png"} alt= {'Experiance      Image'}width={400} height={400} 
+      className=" absolute -top-4 right-80 opacity-70 lg:hidden md:hidden sm:hidden"
+     />
+      <div className="w-full h-full flext flex-col items-center justify-center gap-y-10 lg:gap-y-20">  
+        <div className="w-[600px] xl:w-[480px] sm:w-full px-12 sm:px-0 relative -left-[100px]">
+          <div className=" flex flex-col gap-y-3 rounded-md border border-red-400 bg-white p-4 tracking-wide sm:text-sm">
+            <h1 className="text-xl sm:text-lg font-light text-gray-700">Foundation and Basics </h1>
+            <p>
+              <span>
+                Education:
+              </span>
+              <span>
+                Education:  
+              </span>
+            </p>
+            <div>
+             <span>
+              Experience:
+             </span>
+             <ul>
+              <li>
+                experience
+              </li>
+             </ul>
+               <span>
+                icon
+               </span>
+               <div>
+                2020
+               </div>
+            </div>
+            <div>
+              timeline 
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
